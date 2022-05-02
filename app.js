0d638efd66fb68d384bb6610cf4f4f432d37d5e5
singingw <op0408io1998@gmail.com>
2022-05-02 10:26:22 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const Restaurant = require('./models/restaurant') // 載入 Restaurant model
 const app = express()
 const port = 3000
-//const restaurantList = require('./restaurant.json')
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-app.use(express.static('public'))
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', () => {
@@ -22,7 +23,6 @@ db.once('open', () => {
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
-
 // 設置路由、回應
 app.get('/', (req, res) => {
   Restaurant.find()
@@ -30,17 +30,15 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
-
 app.get('/restaurant/new', (req, res) => {
   return res.render('new')
 })
 
-app.post('/restaurants', (req, res) => {
+app.put('/restaurants', (req, res) => {
   return Restaurant.create(req.body)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
   // $regex 提供了在查詢 (query) 中找到符合的字串
@@ -65,14 +63,13 @@ app.get('/:restaurant_id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+app.delete('/restaurants/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
   return Restaurant.findById(id)
     .then(restaurant => restaurant.remove())
     .then((restaurant) => res.redirect('/'))
     .catch(error => console.log(error))
 })
-
 app.get('/restaurants/:restaurant_id/edit', (req, res) => {
   const id = req.params.restaurant_id
   return Restaurant.findById(id)
@@ -80,7 +77,8 @@ app.get('/restaurants/:restaurant_id/edit', (req, res) => {
     .then((restaurant) => res.render('edit', { restaurant }))
     .catch(error => console.log(error))
 })
-app.post('/restaurants/:restaurant_id/edit', (req, res) => {
+
+app.put('/restaurants/:restaurant_id', (req, res) => {
   const id = req.params.restaurant_id
   const updatedRest = req.body
   return Restaurant.findById(id)
