b8830ff2ca8b8f249d912c30335cc399947be852
singingw <op0408io1998@gmail.com>
2022-05-02 23:18:51 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -8,4 +8,22 @@ router.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
+router.post('/filter', (req, res) => {
+  const { sort, category, rating } = req.body
+  const sortRule = {
+    default: { _id: 'asc' },
+    name_asc: { name: 'asc' },
+    name_desc: { name: 'desc' },
+    rating_asc: { rating: 'asc' },
+    rating_desc: { rating: 'desc' }
+  }
+  Restaurant.find({
+    $and: [{ category: { $regex: category, $options: 'i' } },
+    { rating: { $gte: rating } }]
+  })
+    .sort(sortRule[sort])
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, sort, category, rating }))
+    .catch(error => console.log(error))
+})
 module.exports = router
