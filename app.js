64b4f9e64cd98b31c8c04583f957de1ea299eb88
singingw <op0408io1998@gmail.com>
2022-06-12 18:33:49 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const session = require('express-session')
-const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 const port = process.env.PORT || 3000
 const app = express()
 
-usePassport(app)
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -23,6 +23,12 @@ app.use(session({
   saveUninitialized: true
 }))
 
+usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
-const { authenticator } = require('../middleware/auth')  
+const { authenticator } = require('../middleware/auth')
 
 router.use('/restaurant/', authenticator, restaurant)
 router.use('/users', users)
 router.use('/', authenticator, home)
+
 // 匯出路由器
 module.exports = router
