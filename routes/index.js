e7887fbacff9f243534b638916874b2d9a7afa52
singingw <op0408io1998@gmail.com>
2022-06-12 14:30:39 +0800

    feat: add register login  page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
-
+const users = require('./modules/users')
 router.use('/', home)
 router.use('/restaurant/', restaurant)
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
