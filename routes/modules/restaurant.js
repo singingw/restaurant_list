c350f30f05f2cfd5c2eb964a3e042217ae971461
singingw <op0408io1998@gmail.com>
2022-06-13 22:24:53 +0800

    feat: add .env.example

--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -6,11 +6,10 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 router.post('/', (req, res) => {
-  const userId = req.user._id
-  const { name, category, image, location, phone, rating, description } = req.body
-  return Restaurant.create({ name, category, image, location, phone, rating, description, userId })
+  req.body.userId = req.user._id
+  return Restaurant.create(req.body)
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch(err => console.error(err))
 })
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
@@ -41,7 +40,7 @@ router.delete('/:restaurant_id', (req, res) => {
   const userId = req.user._id
   return Restaurant.findOne({ _id, userId })
     .then(restaurant => restaurant.remove())
-    .then((restaurant) => res.redirect('/'))
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 router.get('/:restaurant_id/edit', (req, res) => {
@@ -54,22 +53,9 @@ router.get('/:restaurant_id/edit', (req, res) => {
 })
 router.put('/:restaurant_id', (req, res) => {
   const _id = req.params.restaurant_id
-  const userId = req.user._id
-  const updatedRest = req.body
-  return Restaurant.findOne({ _id, userId })
-    .then(restaurant => {
-      restaurant.name = updatedRest.name
-      restaurant.name_en = updatedRest.name_en
-      restaurant.category = updatedRest.category
-      restaurant.image = updatedRest.image
-      restaurant.location = updatedRest.location
-      restaurant.phone = updatedRest.phone
-      restaurant.google_map = updatedRest.google_map
-      restaurant.rating = updatedRest.rating
-      restaurant.description = updatedRest.description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurant/${id}`))
-    .catch(error => console.log(error))
+  req.body.userId = req.user._id
+  return Restaurant.findOneAndUpdate(_id, req.body)
+    .then(() => res.redirect(`/restaurant/${_id}`))
+    .catch(error => console.error(error))
 })
 module.exports = router
