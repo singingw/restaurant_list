62b153bc8bb8c65d507f83bc4ef64d83012281e7
singingw <op0408io1998@gmail.com>
2022-06-16 23:10:58 +0800

    寫入資料庫的行為，改為解構賦值

--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -7,7 +7,8 @@ router.get('/new', (req, res) => {
 })
 router.post('/', (req, res) => {
   req.body.userId = req.user._id
-  return Restaurant.create(req.body)
+  const { name, name_en, category, image, location, phone, google_map, rating, description, userId } = req.body
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
     .then(() => res.redirect('/'))
     .catch(err => console.error(err))
 })
@@ -56,9 +57,22 @@ router.get('/:restaurant_id/edit', (req, res) => {
 })
 router.put('/:restaurant_id', (req, res) => {
   const _id = req.params.restaurant_id
-  req.body.userId = req.user._id
-  return Restaurant.findOneAndUpdate(_id, req.body)
+  const userId = req.user._id
+  const updatedRest = req.body
+  return Restaurant.findOne({ _id, userId })
+    .then(restaurant => {
+      restaurant.name = updatedRest.name
+      restaurant.name_en = updatedRest.name_en
+      restaurant.category = updatedRest.category
+      restaurant.image = updatedRest.image
+      restaurant.location = updatedRest.location
+      restaurant.phone = updatedRest.phone
+      restaurant.google_map = updatedRest.google_map
+      restaurant.rating = updatedRest.rating
+      restaurant.description = updatedRest.description
+      return restaurant.save()
+    })
     .then(() => res.redirect(`/restaurant/${_id}`))
-    .catch(error => console.error(error))
+    .catch(error => console.log(error))
 })
 module.exports = router
